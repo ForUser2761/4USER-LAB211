961fe123fd67d14faf09adaff3959f24e1fd48c6
foruser <vinhpham2761.gmail.com>
2024-01-04 16:04:57 +0700

    condition + loop

---
+++ Condition/src/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int day = 1;
+
+        switch (day) {
+            case 1:
+                System.out.println("Thứ Hai");
+                
+            case 2:
+                System.out.println("Thứ Ba");
+                
+            case 3:
+                System.out.println("Thứ Tư");
+                break;
+            default:
+                System.out.println("Không xác định");
+        }
+    }
+}
---
+++ Loop/src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (true) {
+            System.out.println(i + ". Anh xin lỗi em");
+            i++;
+        }
+    }
+}
